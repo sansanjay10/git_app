ec2a970b28c8b1ba96ce09e9257ac950032422dd
Santhana Sanjay G S <116257653+sansanjay10@users.noreply.github.com>
2024-09-29 21:12:15 +0530

    Update git.js

--- git.js
+++ git.js
@@ -1,19 +1,23 @@
 function git() {
-
-    const orignalName =document.getElementById("text").value;
-    
+    var orignalName = document.getElementById("text").value;
     console.log(orignalName);
     
     fetch("https://api.github.com/users/" + orignalName)
-    
     .then((result) => result.json())
-    
     .then((data) => {
-    
-    console.log(data);
-    
-    document.getElementById("result")
-    .innerHTML=`<img src="${data.avatar_url}" alt="user_avatar"><br>ID: ${data.login}<br> <h1>Total Repo: ${data.public_repos}</h1>
-    <br><h1>Name: ${data.name} </h1><br> <h1>Location: ${data.location} </h1>`;
-});
+        console.log(data);
+        document.getElementById("result").innerHTML = `
+            <img src="${data.avatar_url}" alt="user_avatar" class="avatar"><br>
+            <span class="username">${data.login}</span><br><br>
+            <div>
+                <span class="label">Name:</span> <span class="value">${data.name}</span><br><br>
+                <span class="label">Location:</span> <span class="value">${data.location}</span><br><br>
+                <span class="label">Total Repositories:</span> <span class="value">${data.public_repos}</span><br><br>
+            </div>
+        `;
+         document.querySelector('.input-container').style.display = 'none';
+    })
+    .catch(error => {
+        document.getElementById("result").innerHTML = `<p>User not found</p>`;
+    });
 }
